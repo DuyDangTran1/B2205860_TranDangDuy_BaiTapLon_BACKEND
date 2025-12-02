2744ddfd9d6e62eae49f1b26a9a51878eb6adee8
DangDuyTran1 <tuechst1@gmail.com>
2025-12-02 19:12:53 +0700

    Chuc cac phuong thuc du lieu cua doc gia

---
+++ Model/SessionModel.js
@@ -0,0 +1,49 @@
+const {
+  connectDB,
+  getCollection,
+} = require("../ConnectMongoDB/ConnectMongoDB");
+
+const createRefreshToken = async (session) => {
+  await connectDB();
+  const sessionCollection = await getCollection("Session");
+  await sessionCollection.createIndex(
+    { expiresAt: 1 },
+    { expireAfterSeconds: 0 }
+  );
+
+  return await sessionCollection.insertOne(session);
+};
+
+const isExistSession = async (email) => {
+  await connectDB();
+  const sessionCollection = await getCollection("Session");
+  if (!(await sessionCollection.findOne({ email: email }))) return false;
+
+  return true;
+};
+
+const findSessionByEmail = async (email) => {
+  await connectDB();
+  const sessionCollection = await getCollection("Session");
+  return await sessionCollection.findOne({ email: email });
+};
+
+const findSessionByRefresh = async (refresh) => {
+  await connectDB();
+  const sessionCollection = await getCollection("Session");
+  return await sessionCollection.findOne({ refresh_token: refresh });
+};
+
+const removeSessionByEmail = async (user) => {
+  await connectDB();
+  const sessionCollection = await getCollection("Session");
+  return await sessionCollection.deleteOne({ email: user.EMAIL });
+};
+
+module.exports = {
+  createRefreshToken,
+  isExistSession,
+  findSessionByEmail,
+  findSessionByRefresh,
+  removeSessionByEmail,
+};
